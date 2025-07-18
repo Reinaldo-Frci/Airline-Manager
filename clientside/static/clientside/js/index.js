3cb8c9be2dda764b46707dd62cc5ef8a269e0bd6
Reinaldo F. <reinaldo.frci@gmail.com>
2025-07-17 22:01:05 -0300

    fix: changes to login popup

--- clientside/static/clientside/js/index.js
+++ clientside/static/clientside/js/index.js
@@ -1,8 +1,10 @@
 function popup(){
-  let popup = document.getElementById("myPopup");
+  var popup = document.getElementById("loginPopup");
   popup.classList.toggle("show");
 }
 
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("loginButton").onclick = popup;
+    document.getElementById("loginPopup").onsubmit = popup;
+/*    document.getElementById("loginPopup").onclick = popup; */
 })
